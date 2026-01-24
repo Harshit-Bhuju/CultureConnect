dce34e7cd3c0ba88f6c9cc194523c81e5f6e8354
Harman Bhuju <harmanbhuju@gmail.com>
2026-01-24 09:22:20 +0545

    deleted unnecessary files and folders

--- backend/admin_panel/default_image_send.php
+++
@@ -1,4 +0,0 @@
-<?php
-include("../header.php");
-$defaultImage = 'http://localhost/CultureConnect/backend/uploads/default-image.jpg';
-echo json_encode(['status' => 'success', 'default_image' => $defaultImage]);
--- backend/admin_panel/welcome_send.php
+++
@@ -1,2 +0,0 @@
-<?php
-include("../get_welcome.php");
